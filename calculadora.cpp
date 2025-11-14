77e1875701166ba4e7d80d0044e9a964bba42870
Trujo666 <alejandrotrujillogalan@gmail.com>
2025-11-14 19:27:28 +0100

    feat: añadir función potencia y llamada en main

--- calculadora.cpp
+++ calculadora.cpp
@@ -3,5 +3,11 @@
  int restar(int a, int b) { return a - b; }
  int multiplicar(int a, int b) { return a * b; }
  int dividir(int a, int b) { return (b != 0) ? a / b : 0; }
- int potencia(int a, int b) { return pow(a, b); }
+ int potencia(int a, int b) {
+    int resultado = 1;
+    for (int i = 0; i < b; i++) {
+        resultado *= a;
+    }
+    return resultado;
+}
  
